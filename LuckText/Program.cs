cffff165e075ba8a0158f21210ee297e81ae3ef8
Brian Nwaijah <brian.nwaijah@elev.ga.ntig.se>
2024-08-30 13:58:51 +0200

    Första

---
+++ LuckText/Program.cs
@@ -0,0 +1,21 @@
+﻿string name;
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Skriv in ditt namn: ");
+
+name = Console.ReadLine();
+
+string food;
+Console.WriteLine("Favorit mat: ");
+
+food = Console.ReadLine();
+
+string sport;
+Console.WriteLine("Favorit sport: ");
+
+sport = Console.ReadLine();
+Console.Clear();    
+Console.WriteLine($"Hello my name is {name} and i like to eat {food} while i watch {sport} in my bed");
+
+Console.ReadLine();
+
